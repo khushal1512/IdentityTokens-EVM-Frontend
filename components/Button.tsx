2ad814569a90c93dc428a82f3fc7c15ccd2a1ca3
khushal1512 <khushal080424@gmail.com>
2026-02-04 12:35:16 +0530

    (fix): remove duplicate cn() function from Button.tsx

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,11 +1,5 @@
 import React from "react";
-import { clsx, type ClassValue } from "clsx";
-import { twMerge } from "tailwind-merge";
-
-function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
-}
-
+import { cn } from "../lib/utils";
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   variant?: "primary" | "secondary" | "outline";
   size?: "sm" | "md" | "lg";
